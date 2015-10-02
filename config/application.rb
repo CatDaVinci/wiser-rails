4965a741ff34e9ac0bbb726fa8af1b900c169906
Vladimir Mozgin <mozgin.vova@gmail.com>
2015-10-02 09:44:53 +0300

    add cors config and no helper and assets generate config

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,19 @@ Bundler.require(*Rails.groups)
 
 module Wiser
   class Application < Rails::Application
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

8784bec088c3bad19ff812c27c175d83fb40c0d1
Vladimir Mozgin <mozgin.vova@gmail.com>
2015-10-03 11:51:36 +0300

    refactoring test

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -1,51 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ProductsController, :type => :controller do
-  let!(:products) { create_list(:product, 100) }
+  before do
+    products if defined?(products)
+    request
+  end
+
+  let(:response_body) { JSON.parse response.body }
 
   describe "GET index" do
-    let(:index_request) { get :index }
-    let(:index_response) { JSON.parse response.body }
+    let(:request) { get :index }
+    let(:products) { create_list(:product, 100) }
 
     context "response" do
-      before { index_request }
-
-      it 'should have status ok' do
-        expect(response).to have_http_status(:ok)
-      end
-
-      it 'have products' do
-        expect(index_response["products"].count).to eq 100
-      end
+      it('should have status ok') { expect(response).to have_http_status(:ok) }
+      it('have products') { expect(response_body["products"].count).to eq 100 }
     end
   end
 
   describe "PUT update" do
-    let!(:product_attr) { attributes_for(:product) }
-    let!(:product) { create(:product) }
-    let(:update_request) { put :update, id: product.id,
-                            product: product_attr }
-    let(:update_response) { JSON.parse response.body }
+    let(:product_attrs) { attributes_for(:product) }
+    let(:product) { create(:product) }
+    let(:request) { put :update, id: product.id, product: product_attrs }
 
-    before { update_request }
-
-    context 'response' do
-      it 'should have status updated' do
-        expect(response).to have_http_status(:accepted)
-      end
-
-      context 'should have product' do
-        it { expect(update_response['product']['title']).to eq product_attr[:title] }
-        it { expect(update_response['product']['sku']).to eq product_attr[:sku] }
-        it { expect(update_response['product']['category']).to eq product_attr[:category] }
-      end
-    end
-
-    context 'when product update' do
-      it { expect(Product.find(product.id)['title']).to eq product_attr[:title] }
-      it { expect(Product.find(product.id).sku).to eq product_attr[:sku] }
-      it { expect(Product.find(product.id).category).to eq product_attr[:category] }
-    end
-
+    it('should have status updated') { expect(response).to have_http_status(:accepted) }
+    it { expect(response_body['product']['title']).to eq product_attrs[:title] }
+    it { expect(response_body['product']['sku']).to eq product_attrs[:sku] }
+    it { expect(response_body['product']['category']).to eq product_attrs[:category] }
+    it { expect(product.reload.title).to eq product_attrs[:title] }
+    it { expect(product.reload.sku).to eq product_attrs[:sku] }
+    it { expect(product.reload.category).to eq product_attrs[:category] }
   end
 end

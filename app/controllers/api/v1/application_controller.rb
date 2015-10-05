cf57e9d58b59a724254e93c387b24333859a3190
Vladimir Mozgin <mozgin.vova@gmail.com>
2015-10-05 17:08:16 +0300

    add header in pagination

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -6,6 +6,13 @@ class Api::V1::ApplicationController < ApplicationController
   end
 
   def render_success(object = {}, status = :ok, options = {})
+    if object.respond_to?(:total_entries)
+      headers['X-Pagination'] = {
+        current_page: object.current_page,
+        per_page: object.per_page,
+        total_pages: object.total_pages
+      }.to_json
+    end
     render options.merge({:json => object, :status => status})
   end
 
